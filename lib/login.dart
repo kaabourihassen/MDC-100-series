cbdbac0a36aedee74a645fa2ccc8c69b928586c4
Hassen Kaabouri <kaabouri.hassen704@gmail.com>
2021-11-13 13:49:38 +0100

    MDC-103 : try another theme

--- lib/login.dart
+++ lib/login.dart
@@ -13,6 +13,7 @@
 // limitations under the License.
 
 import 'package:flutter/material.dart';
+import 'colors.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -55,7 +56,7 @@ class _LoginPageState extends State<LoginPage> {
             const SizedBox(height: 80.0),
             Column(
               children: <Widget>[
-                Image.asset('assets/diamond.png'),
+                Image.asset('assets/diamond.png', color: kShrineBlack),
                 const SizedBox(height: 16.0),
                 Text(
                   'SHRINE',
