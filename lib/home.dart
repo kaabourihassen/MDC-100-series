3903c3525b3173959f588b06e16209c16cbba205
Hassen Kaabouri <kaabouri.hassen704@gmail.com>
2021-11-13 15:19:24 +0100

    MDC-104 : add the backdrop menu

--- lib/home.dart
+++ lib/home.dart
@@ -11,6 +11,8 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: Return an AsymmetricView (104)
+    return AsymmetricView(products: ProductsRepository.loadProducts(Category.all));
+
     // TODO: Pass Category variable to AsymmetricView (104)
     return Scaffold(
       appBar: AppBar(
